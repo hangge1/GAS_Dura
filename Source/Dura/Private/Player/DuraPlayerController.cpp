45fd06d01edb1d57e57d9dddc7b89e96c7e6b2c9
hangdada <824362330@qq.com>
2024-10-02 19:57:29 +0800

    BindAction Move And Linking Logic

--- Source/Dura/Private/Player/DuraPlayerController.cpp
+++ Source/Dura/Private/Player/DuraPlayerController.cpp
@@ -3,6 +3,9 @@
 
 #include "Player/DuraPlayerController.h"
 #include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+
+
 
 ADuraPlayerController::ADuraPlayerController()
 {
@@ -26,4 +29,32 @@ void ADuraPlayerController::BeginPlay()
 	Inputmode.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
 	Inputmode.SetHideCursorDuringCapture(false);
 	SetInputMode(Inputmode);
+}
+
+void ADuraPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	UEnhancedInputComponent* inputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+	check(MoveAction);
+
+	inputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ADuraPlayerController::Move);
+}
+
+void ADuraPlayerController::Move(const FInputActionValue& InputValue)
+{
+	FVector2d AxisValue = InputValue.Get<FVector2d>();
+
+	FRotator CtlRotation = GetControlRotation();
+	CtlRotation.Pitch = 0.0f;
+	CtlRotation.Roll = 0.0f;
+
+	FVector ForwardVector = FRotationMatrix(CtlRotation).GetUnitAxis(EAxis::Y);
+	FVector RightVector = FRotationMatrix(CtlRotation).GetUnitAxis(EAxis::X);
+
+	if (APawn* ControlPawn = GetPawn())
+	{
+		ControlPawn->AddMovementInput(ForwardVector, AxisValue.Y);
+		ControlPawn->AddMovementInput(RightVector, AxisValue.X);
+	}
 }
--- Source/Dura/Public/Player/DuraPlayerController.h
+++ Source/Dura/Public/Player/DuraPlayerController.h
@@ -7,6 +7,8 @@
 #include "DuraPlayerController.generated.h"
 
 class UInputMappingContext;
+struct FInputActionValue;
+class UInputAction;
 
 /**
  * 
@@ -22,7 +24,17 @@ public:
 protected:
 	virtual void BeginPlay() override;
 
+	virtual void SetupInputComponent() override;
+
+private:
+	void Move(const FInputActionValue& InputValue);
+
 private:
 	UPROPERTY(EditAnywhere, Category="Input")
 	TObjectPtr<UInputMappingContext> DuraContext;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputAction> MoveAction;
+
+
 };
