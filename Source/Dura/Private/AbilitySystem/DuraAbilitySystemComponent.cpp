9e0a85ec7989a29988379565b9574ffb5f18d899
hangdada <824362330@qq.com>
2024-10-12 12:57:54 +0800

    Add AttributeInfo DataAsset

--- Source/Dura/Private/AbilitySystem/DuraAbilitySystemComponent.cpp
+++ Source/Dura/Private/AbilitySystem/DuraAbilitySystemComponent.cpp
@@ -9,12 +9,6 @@
 void UDuraAbilitySystemComponent::AbilityActorInfoSet()
 {
 	OnGameplayEffectAppliedDelegateToSelf.AddUObject(this, &UDuraAbilitySystemComponent::EffectApplied);
-
-	const auto& Tags = FDuraGameplayTags::Get();
-	GEngine->AddOnScreenDebugMessage(-1, 10.0f, FColor::Orange, 
-		FString::Printf(TEXT("Tag: %s"), *Tags.Attributes_Secondary_Armor.ToString())
-	);
-
 }
 
 void UDuraAbilitySystemComponent::EffectApplied(UAbilitySystemComponent* AbilitySystemComponent, const FGameplayEffectSpec& EffectSpec, FActiveGameplayEffectHandle ActiveEffectHandle)
--- Source/Dura/Private/DuraGameplayTags.cpp
+++ Source/Dura/Private/DuraGameplayTags.cpp
@@ -8,5 +8,65 @@ FDuraGameplayTags FDuraGameplayTags::DuraGameplayTags;
 
 void FDuraGameplayTags::InitializeNativeGameplayTags()
 {
-	DuraGameplayTags.Attributes_Secondary_Armor = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Secondary.Armor"), FString("Reduces damage taken, improves Block Chance"));
+	/*
+	 * Primary Attributes
+	 */
+	DuraGameplayTags.Attributes_Primary_Strength = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attributes.Primary.Strength"),
+		FString("Increases physical damage"));
+
+	DuraGameplayTags.Attributes_Primary_Intelligence = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attributes.Primary.Intelligence"),
+		FString("Increases magical damage"));
+
+	DuraGameplayTags.Attributes_Primary_Resilience = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attributes.Primary.Resilience"),
+		FString("Increases Armor and Armor Penetration"));
+
+	DuraGameplayTags.Attributes_Primary_Vigor = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attributes.Primary.Vigor"),
+		FString("Increases Health"));
+
+	/*
+	 * Secondary Attributes
+	 */
+	DuraGameplayTags.Attributes_Secondary_Armor = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attributes.Secondary.Armor"), 
+		FString("Reduces damage taken, improves Block Chance"));
+
+	DuraGameplayTags.Attributes_Secondary_Armor_Penetration = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attributes.Secondary.Armor_Penetration"), 
+		FString("Reduces damage taken, improves Block Chance"));
+
+	DuraGameplayTags.Attributes_Secondary_Block_Chance = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attributes.Secondary.Block_Chance"), 
+		FString("Reduces damage taken, improves Block Chance"));
+
+	DuraGameplayTags.Attributes_Secondary_CriticalHitChance = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attributes.Secondary.CriticalHitChance"), 
+		FString("Reduces damage taken, improves Block Chance"));
+
+	DuraGameplayTags.Attributes_Secondary_CriticalHitDamage = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attributes.Secondary.CriticalHitDamage"), 
+		FString("Reduces damage taken, improves Block Chance"));
+
+	DuraGameplayTags.Attributes_Secondary_CriticalHitResistance = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attributes.Secondary.CriticalHitResistance"), 
+		FString("Reduces damage taken, improves Block Chance"));
+
+	DuraGameplayTags.Attributes_Secondary_HealthRegeneration = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attributes.Secondary.HealthRegeneration"), 
+		FString("Reduces damage taken, improves Block Chance"));
+
+	DuraGameplayTags.Attributes_Secondary_ManaRegeneration = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attributes.Secondary.ManaRegeneration"), 
+		FString("Reduces damage taken, improves Block Chance"));
+
+	DuraGameplayTags.Attributes_Secondary_MaxHealth = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attributes.Secondary.MaxHealth"), 
+		FString("Reduces damage taken, improves Block Chance"));
+
+	DuraGameplayTags.Attributes_Secondary_MaxMana = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attributes.Secondary.MaxMana"), 
+		FString("Reduces damage taken, improves Block Chance"));
 }
