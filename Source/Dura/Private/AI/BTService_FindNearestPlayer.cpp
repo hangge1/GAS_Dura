eba549392e89e5cacba0d7652648f8131def7cec
hangdada <824362330@qq.com>
2024-10-29 22:46:23 +0800

    Add Find Nearest Player Service

---
+++ Source/Dura/Private/AI/BTService_FindNearestPlayer.cpp
@@ -0,0 +1,16 @@
+// Copyright by person HDD  
+
+
+#include "AI/BTService_FindNearestPlayer.h"
+
+#include "AIController.h"
+
+void UBTService_FindNearestPlayer::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+
+	GEngine->AddOnScreenDebugMessage(1, 1.f, FColor::Red, *AIOwner->GetName());
+	GEngine->AddOnScreenDebugMessage(2, 1.f, FColor::Red, *ActorOwner->GetName());
+
+
+}
