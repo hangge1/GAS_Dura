0b94f24d8463178de9f4407dec05cf32c0ae70f0
hangdada <824362330@qq.com>
2024-11-21 03:07:13 +0800

    Fix BP_FireBolt Expolte too early buyg

--- Source/Dura/Public/Actor/DuraProjectile.h
+++ Source/Dura/Public/Actor/DuraProjectile.h
@@ -33,14 +33,14 @@ protected:
 
 	UFUNCTION()
 	void OnSphereOverlap(UPrimitiveComponent* OverlamppedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<USphereComponent> Sphere;
 private:
 	bool bHit = false;
 
 	UPROPERTY(EditAnywhere)
 	float LifeSpan = 15.f;
-
-	UPROPERTY(VisibleAnywhere)
-	TObjectPtr<USphereComponent> Sphere;
 
 	UPROPERTY(EditAnywhere)
 	TObjectPtr<UNiagaraSystem> ImpactEffect;
