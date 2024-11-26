b145940b8498d01e9f1d2c11b4194ea25d3738a3
hangdada <824362330@qq.com>
2024-11-26 14:35:08 +0800

    Add XP in the player state

--- Source/Dura/Private/Player/DuraPlayerState.cpp
+++ Source/Dura/Private/Player/DuraPlayerState.cpp
@@ -23,12 +23,44 @@ UAbilitySystemComponent* ADuraPlayerState::GetAbilitySystemComponent() const
 	return AbilitiesSystemComponent;
 }
 
+void ADuraPlayerState::AddToXP(int32 InXP)
+{
+    XP += InXP;
+    OnXPChangedDelegate.Broadcast(XP);
+}
+
+void ADuraPlayerState::AddToLevel(int32 InLevel)
+{
+    Level += InLevel;
+    OnLevelChangedDelegate.Broadcast(Level);
+}
+
+void ADuraPlayerState::SetXP(int32 InXP)
+{
+    XP = InXP;
+    OnXPChangedDelegate.Broadcast(XP);
+}
+
+void ADuraPlayerState::SetLevel(int32 InLevel)
+{
+    Level = InLevel;
+    OnLevelChangedDelegate.Broadcast(Level);
+}
+
 void ADuraPlayerState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
 {
+    Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
 	DOREPLIFETIME(ADuraPlayerState, Level);
+	DOREPLIFETIME(ADuraPlayerState, XP);
 }
 
 void ADuraPlayerState::OnRep_Level(int32 OldLevel)
 {
+    OnLevelChangedDelegate.Broadcast(Level);
+}
 
+void ADuraPlayerState::OnRep_XP(int32 OldXP)
+{
+    OnXPChangedDelegate.Broadcast(XP);
 }
--- Source/Dura/Public/Player/DuraPlayerState.h
+++ Source/Dura/Public/Player/DuraPlayerState.h
@@ -11,6 +11,8 @@
 class UAbilitySystemComponent;
 class UAttributeSet;
 
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnPlayerStatChanged, int32 /*StatValue*/);
+
 /**
  * 
  */
@@ -22,12 +24,24 @@ class DURA_API ADuraPlayerState : public APlayerState, public IAbilitySystemInte
 public:
 	ADuraPlayerState();
 
+    virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
 	UAbilitySystemComponent* GetAbilitySystemComponent() const override;
 	UAttributeSet* GetAttributeSet() const { return AttributeSet; }
 
-	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	
+    FOnPlayerStatChanged OnXPChangedDelegate;
+    FOnPlayerStatChanged OnLevelChangedDelegate;
 
-	int32 GetPlayerLevel() const { return Level; }
+    FORCEINLINE int32 GetPlayerLevel() const { return Level; }
+    FORCEINLINE int32 GetXP() const { return XP; }
+
+    void AddToXP(int32 InXP);
+    void AddToLevel(int32 InLevel);
+
+
+    void SetXP(int32 InXP);
+    void SetLevel(int32 InLevel);
 protected:
 	UPROPERTY(VisibleAnywhere)
 	TObjectPtr<UAbilitySystemComponent> AbilitiesSystemComponent;
@@ -35,9 +49,16 @@ protected:
 	UPROPERTY()
 	TObjectPtr<UAttributeSet> AttributeSet;
 
+private:
 	UPROPERTY(VisibleAnywhere, ReplicatedUsing = OnRep_Level);
 	int32 Level = 1;
 
+    UPROPERTY(VisibleAnywhere, ReplicatedUsing = OnRep_XP);
+	int32 XP = 1;
+
 	UFUNCTION()
 	void OnRep_Level(int32 OldLevel);
+
+    UFUNCTION()
+	void OnRep_XP(int32 OldXP);
 };
