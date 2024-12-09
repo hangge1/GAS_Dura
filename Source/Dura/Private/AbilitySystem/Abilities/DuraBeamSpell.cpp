aad421565c36217d75b18d58b127f1e7a0e38522
hangdada <824362330@qq.com>
2024-12-09 17:32:51 +0800

    First Trace Target

--- Source/Dura/Private/AbilitySystem/Abilities/DuraBeamSpell.cpp
+++ Source/Dura/Private/AbilitySystem/Abilities/DuraBeamSpell.cpp
@@ -3,6 +3,8 @@
 
 #include "AbilitySystem/Abilities/DuraBeamSpell.h"
 #include "GameFramework/Character.h"
+#include <Interaction\CombatInterface.h>
+#include <Kismet\KismetSystemLibrary.h>
 
 void UDuraBeamSpell::StoreMouseDataInfo(const FHitResult& HitResult)
 {
@@ -25,4 +27,38 @@ void UDuraBeamSpell::StoreOwnerVariables()
         OwnerCharacter = Cast<ACharacter>(CurrentActorInfo->AvatarActor);
         OwnerPlayerController = CurrentActorInfo->PlayerController.Get();
     }
+}
+
+void UDuraBeamSpell::TraceFirstTarget(const FVector& BeamTargetLocation)
+{
+    check(OwnerCharacter);
+    if(OwnerCharacter->Implements<UCombatInterface>())
+    {
+        if(USkeletalMeshComponent* Weapon = ICombatInterface::Execute_GetWeapon(OwnerCharacter))
+        {
+            TArray<AActor*> ActorsToIgnore;
+            ActorsToIgnore.Add(OwnerCharacter);
+            FHitResult HitResult;
+            const FVector SocketLocation = Weapon->GetSocketLocation(FName("TipSocket"));
+            UKismetSystemLibrary::SphereTraceSingle(
+                OwnerCharacter, 
+                SocketLocation, 
+                BeamTargetLocation, 
+                10.f, 
+                TraceTypeQuery1, 
+                false, 
+                ActorsToIgnore, 
+                EDrawDebugTrace::None, 
+                HitResult, 
+                true
+            );
+
+            if(HitResult.bBlockingHit)
+            {
+                MouseHitLocation = HitResult.ImpactPoint;
+                MouseHitActor = HitResult.GetActor();
+            }
+        }
+    }
+    
 }
